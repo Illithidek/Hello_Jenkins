4ce9ac9996f13bb78c8687b159a53f8e6d87d8e2
illithid <kalinowskii.michal@gmail.com>
2023-05-24 14:24:39 +0200

    AAA

---
+++ main-test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+class Test: public ::testing::Test{ };
+
+TEST_F(Test, goodTest){
+  auto first = 1;
+  auto second = 1;
+  EXPECT_EQ(first, second);
+}
+
+TEST_F(Test, badTest){
+  auto first = 0;
+  auto second = 1;
+  EXPECT_EQ(first, second);
+}
